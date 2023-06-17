9d7ffe6f19fb8e6e77e5b196846a9be82f6905dd
Adel-George <adel.george.isaac@gmail.com>
2023-06-17 22:09:00 +0300

    add : total Page to product

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -16,7 +16,11 @@ exports.getAllProducts = async function (request, response, next) {
       .skip(skip)
       .limit(limit);
     if (allProducts.length == 0) throw new Error("No Products exist");
-    response.status(200).json({message: "Done", page, allProducts });
+    let totalPages = await Product.countDocuments({});
+    totalPages = Math.ceil(totalPages / limit);
+    response
+      .status(200)
+      .json({ message: "Done", page, allProducts, totalPages });
   } catch (error) {
     next(error);
   }
@@ -44,7 +48,7 @@ exports.createProduct = async (request, response, next) => {
 
     // Save the product to the database
     const newProduct = await product.save();
-    response.status(201).json({message: "Done",newProduct});
+    response.status(201).json({ message: "Done", newProduct });
   } catch (error) {
     next(error);
   }
@@ -113,7 +117,7 @@ exports.searchProduct = async function (request, response, next) {
     } else {
       const products = await Product.find(query);
       if (products.length == 0) throw new Error("No Product exist");
-      response.status(200).json({message: "Done",products});
+      response.status(200).json({ message: "Done", products });
     }
   } catch (error) {
     next(error);
@@ -137,7 +141,7 @@ exports.updateProductById = async function (request, response, next) {
 
     if (!product) throw new Error("No product exist by this ID");
 
-    response.status(200).json({ message:"Done", product });
+    response.status(200).json({ message: "Done", product });
   } catch (error) {
     next(error);
   }
@@ -169,7 +173,7 @@ exports.deleteProductById = async function (request, response, next) {
       }
     }
 
-    response.status(200).json({ message:"Done", product });
+    response.status(200).json({ message: "Done", product });
   } catch (error) {
     next(error);
   }
