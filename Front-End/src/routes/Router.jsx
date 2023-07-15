a8e37a2b754b0bdbf1ebe8cd73e6150e96963e41
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2023-07-15 20:17:55 +0300

    fix: deactivate lazy loading

--- Front-End/src/routes/Router.jsx
+++ Front-End/src/routes/Router.jsx
@@ -11,52 +11,31 @@ import ConfirmEmail from "../components/ConfirmEmail";
 import UserGaurdRouter from "./UserGaurdRouter.jsx";
 import { Home } from "../pages/Home.jsx";
 import { HomeGuard, LoginGuard } from "../guards/AuthGuards.js";
+import AddUser from "../pages/AdminPages/AddUser";
+import AddMovie from "../pages/AdminPages/AddMovie";
+import AddProduct from "../pages/AdminPages/AddProduct";
+import UserList from "../pages/AdminPages/UserList";
+import MoviesList from "../pages/AdminPages/MoviesList";
+import ProductsList from "../pages/AdminPages/ProductsList";
+import OrdersList from "../pages/AdminPages/OrdersList";
+import ReviewsList from "../pages/AdminPages/ReviewsList";
+
+// E-Commerce
+import Cart from "./../pages/ProductPages/Cart";
+import Store from "./../pages/ProductPages/Store";
+import ProductDetails from "./../pages/ProductPages/ProductDetails";
+import UserOrders from "./../pages/ProductPages/UserOrders";
+//Stream
+import Movies from "../pages/StreamPages/Movies.jsx";
+import MovieDetails from "../pages/StreamPages/moviePage.jsx";
+import Favorites from "../pages/StreamPages/Favorites.jsx";
+import Animes from "../pages/StreamPages/Anime.jsx";
+import Series from "../pages/StreamPages/Series.jsx";
+import ChangePassword from "./../pages/AccountPages/ChangePassword";
+import Profile from "./../pages/AccountPages/Profile";
+import Settings from "../pages/AccountPages/Settings";
+
 export default function Router() {
-  const AddUser = lazy(() => import("../pages/AdminPages/AddUser"));
-  const AddMovie = lazy(() => import("../pages/AdminPages/AddMovie"));
-  const AddProduct = lazy(() => import("../pages/AdminPages/AddProduct"));
-  const UserList = lazy(() => import("../pages/AdminPages/UserList"));
-  const MoviesList = lazy(() => import("../pages/AdminPages/MoviesList"));
-  const ProductsList = lazy(() => import("../pages/AdminPages/ProductsList"));
-  const OrdersList = lazy(() => import("../pages/AdminPages/OrdersList"));
-  const ReviewsList = lazy(() => import("../pages/AdminPages/ReviewsList"));
-
-  // E-Commerce
-  const Cart = lazy(() => import("./../pages/ProductPages/Cart"));
-  const Store = lazy(() => import("./../pages/ProductPages/Store"));
-  const ProductDetails = lazy(() =>
-    import("./../pages/ProductPages/ProductDetails")
-  );
-  const UserOrders = lazy(() =>
-    import("./../pages/ProductPages/UserOrders")
-  );
-  //Stream
-  const Movies = lazy(() =>
-    import("../pages/StreamPages/Movies.jsx")
-  );
-  const MovieDetails = lazy(() =>
-    import("../pages/StreamPages/moviePage.jsx")
-  );
-  const Favorites = lazy(() =>
-    import("../pages/StreamPages/Favorites.jsx")
-  );
-  const Animes = lazy(() =>
-    import("../pages/StreamPages/Anime.jsx")
-  );
-  const Series = lazy(() =>
-    import("../pages/StreamPages/Series.jsx")
-  );
-  const ChangePassword = lazy(() =>
-  import("./../pages/AccountPages/ChangePassword")
-);
-const Profile = lazy(() =>
-import("./../pages/AccountPages/Profile")
-);
-const Settings = lazy(() =>
-import("../pages/AccountPages/Settings")
-);
-
-
   return (
     <>
       <Routes>
@@ -90,14 +69,55 @@ import("../pages/AccountPages/Settings")
           <Route path="/settings" element={<Settings />} />
           <Route path="/user/orders" element={<UserOrders />}></Route>
         </Route>
-        <Route path="/" element={<HomeGuard><Home /></HomeGuard>} />
+        <Route
+          path="/"
+          element={
+            <HomeGuard>
+              <Home />
+            </HomeGuard>
+          }
+        />
 
-
-        <Route path="/login" element={<LoginGuard><Login /></LoginGuard>} />
-        <Route path="/register" element={<LoginGuard><Register /></LoginGuard>} />
-        <Route path="/recoverPassword" element={<LoginGuard><RecoverPassword /></LoginGuard>} />  
-        <Route path="/resetPassword/:id" element={<LoginGuard><ResetPassword /></LoginGuard>} />
-        <Route path="/confirmEmail" element={<LoginGuard><ConfirmEmail /></LoginGuard>} />
+        <Route
+          path="/login"
+          element={
+            <LoginGuard>
+              <Login />
+            </LoginGuard>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <LoginGuard>
+              <Register />
+            </LoginGuard>
+          }
+        />
+        <Route
+          path="/recoverPassword"
+          element={
+            <LoginGuard>
+              <RecoverPassword />
+            </LoginGuard>
+          }
+        />
+        <Route
+          path="/resetPassword/:id"
+          element={
+            <LoginGuard>
+              <ResetPassword />
+            </LoginGuard>
+          }
+        />
+        <Route
+          path="/confirmEmail"
+          element={
+            <LoginGuard>
+              <ConfirmEmail />
+            </LoginGuard>
+          }
+        />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
